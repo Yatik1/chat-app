cb144a68e7a1d08f96400c9301910d9728ec1c10
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-01-03 16:47:43 +0530

    ::api routes

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,26 @@
 const express = require('express')
+const dotenv = require('dotenv')
+const {chats} = require('./data/data')
 
 const app = express()
+dotenv.config()
+
+const PORT = process.env.PORT
 
 app.get('/' , (req,res) => {
     res.send('APi is running on this server ! ')
 })
 
-app.listen(5000 , () => {
-    console.log('server is on !')
+app.get('/api/chat' , (req,res) => {
+    res.send(chats)
+})
+ 
+app.get('/api/chat/:id' , (req,res) => {
+    // console.log(req.params.id)
+    const chatById = chats.find(c => c._id === req.params.id);
+    res.send(chatById)
+})
+
+app.listen(PORT , () => {
+    console.log(`Server is on PORT ${PORT}`)
 })
