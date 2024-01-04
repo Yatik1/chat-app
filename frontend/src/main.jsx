2110cd0e8ad1bcbdbe74dcca25f8b07106951c1f
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-01-04 18:18:38 +0530

    ::UI

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,15 @@ import App from './App.jsx'
 import './index.css' 
 
 import {BrowserRouter} from 'react-router-dom'
+import { ChakraProvider } from '@chakra-ui/react'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>   
+    <BrowserRouter> 
+     <ChakraProvider>
        <App />
+     </ChakraProvider>  
     </BrowserRouter>
   </React.StrictMode>,
 )
