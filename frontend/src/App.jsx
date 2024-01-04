47745fe7fcfcf8635fc6f90c119108c419cb563d
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-01-04 16:35:55 +0530

    ::routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
-import { Button } from '@chakra-ui/react'
-
+// import { Button } from '@chakra-ui/react'
+import {Routes ,Route} from "react-router-dom"
+import { ChatPage, Home } from './pages'
 const App = () => {
   return (
-    <div className='bg-blue-500'>
-        <Button >Button</Button>
+    <div className='bg-blue-950 h-screen text-white'>
+        {/* <Button colorSchema='whiteAlpha'>Button</Button> */}
+
+    <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chats" element={<ChatPage />} />
+    </Routes>
+
     </div>
   )
 }
