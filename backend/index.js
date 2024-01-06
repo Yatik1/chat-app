9c1b514da3dd08d551cccb603a2d37126cd3327d
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-01-06 14:21:57 +0530

    ::index.js

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const cors = require('cors')
-const {chats} = require('./data/data')
+// const {chats} = require('./data/data')
 const connectDB = require('./db/db')
+const userRoutes = require('./routes/userRoutes')
+
 
 const app = express()
+app.use(express.json())
+
 app.use(cors())
 dotenv.config()
 
@@ -16,15 +20,7 @@ app.get('/' , (req,res) => {
     res.send('APi is running on this server ! ')
 })
 
-app.get('/api/chats' , (req,res) => {
-    res.send(chats)
-})
- 
-app.get('/api/chat/:id' , (req,res) => {
-    // console.log(req.params.id)
-    const chatById = chats.find(c => c._id === req.params.id);
-    res.send(chatById)
-})
+app.use('/api/user' , userRoutes)
 
 app.listen(PORT , () => {
     console.log(`Server is on PORT ${PORT}`)
