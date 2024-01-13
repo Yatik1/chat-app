8d4e1dacb56ba898142215a0c10674988a0aa29b
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-01-13 14:04:50 +0530

    ::get chat

--- backend/controllers/chatControllers.js
+++ backend/controllers/chatControllers.js
@@ -1,8 +1,8 @@
-const expressAsyncHandler = require("express-async-handler");
+const asyncHandler = require("express-async-handler");
 const Chat = require("../models/chatModel");
 const User = require("../models/userModel");
 
-const accessChat = expressAsyncHandler(async(req,res) => {
+const accessChat = asyncHandler(async(req,res) => {
     const {userId} = req.body
 
     if(!userId) { 
@@ -45,4 +45,25 @@ const accessChat = expressAsyncHandler(async(req,res) => {
          }
       }
 })
-module.exports = {accessChat}
+
+const fetchChats = asyncHandler(async (req, res) => {
+  try {
+    Chat.find({ users: { $elemMatch: { $eq: req.user._id } } })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
+          path: "latestMessage.sender",
+          select: "name pic email",
+        });
+        res.status(200).send(results);
+      });
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
+module.exports = {accessChat , fetchChats}
