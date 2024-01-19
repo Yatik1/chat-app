a8f5c06c71420d82a34c5e22db7d61135a3eed3f
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-01-19 18:08:46 +0530

    ::managing states

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,29 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
+// import React, { useEffect, useState } from 'react'
+// import axios from 'axios'
+
+import { ChatState } from "../context/ChatProvider"
 
 const ChatPage = () => {
-
-  const [chats,setChats] = useState([])
-
-  useEffect(() => {
-    
-  const fetchChats = async () => {
-    try {
-      const {data} = await axios.get("http://localhost:5000/api/chats")
-      setChats(data)
-    }  catch (error) {
-      console.log("Error occured during data fetch" , error)
-    } 
-  }
-  
-     fetchChats();
-  } ,[]) 
+  const {user} = ChatState()
 
   return (
     <div>
-       {chats.map((chat) => (
-        <div key={chat._id}>{chat.chatName}</div>
-       ))}
+       {user.name}
     </div>
   )
 }
