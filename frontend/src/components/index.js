1fe2476d8353b3d813ea1646af1c2c37ba2f15f3
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-02-05 16:43:57 +0530

    index export

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,7 +1,14 @@
-import Login from "./Login";
-import SignUp from "./SignUp";
+import Login from "./Authentication/Login"
+import SignUp from "./Authentication/SignUp"
+
+import ChatBox from "./misc/ChatBox"
+import MyChats from "./misc/MyChats"
+import SideDrawer from "./misc/SideDrawer"
 
 export {
     Login ,
     SignUp,
+    ChatBox,
+    MyChats,
+    SideDrawer,
 }
