596a0ea1a96c7c3ef6f733ea9018864bf305ea20
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-02-05 16:43:38 +0530

    : chat page

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,14 +1,22 @@
 // import React, { useEffect, useState } from 'react'
 // import axios from 'axios'
+import { Box } from "@chakra-ui/layout";
 
 import { ChatState } from "../context/ChatProvider"
+import { ChatBox, MyChats, SideDrawer } from "../components";
 
 const ChatPage = () => {
   const {user} = ChatState()
 
   return (
-    <div>
-       {user.name}
+    <div style={{width: "100%"}}>
+       {/* {user.name}" */}
+       {user && <SideDrawer />}
+       <Box>
+        {user && <MyChats />}
+        {user && <ChatBox />}
+      </Box>
+
     </div>
   )
 }
