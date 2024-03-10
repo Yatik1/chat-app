07d05f9be5dde35cabd87c1ae1c48b0cc8d93101
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-03-10 15:03:35 +0530

    :: debugg

--- frontend/src/context/ChatProvider.jsx
+++ frontend/src/context/ChatProvider.jsx
@@ -5,7 +5,9 @@ const ChatContext = createContext()
 
 const ChatProvider = ({children}) => {
 
-    const [user,setUser] = useState([])
+    const [user,setUser] = useState(null)
+    const [selectedChat , setSelectedChat] = useState(null)
+    const [chats , setChats] = useState([])
 
     useEffect(() => {
       const userInfo = JSON.parse(localStorage.getItem("userInfo"))
@@ -15,7 +17,7 @@ const ChatProvider = ({children}) => {
     } , [])
 
      return (
-        <ChatContext.Provider value={{user,setUser}}>
+        <ChatContext.Provider value={{user,setUser,selectedChat , setSelectedChat,chats , setChats}}>
             {children}
         </ChatContext.Provider>
      )
