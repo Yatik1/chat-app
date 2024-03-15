733b26a2afc19b5ca2d048046e467f1762e97b70
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-03-15 19:40:16 +0530

    page

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -2,8 +2,10 @@ import { Box } from "@chakra-ui/layout";
 
 import { ChatState } from "../context/ChatProvider"
 import { ChatBox, MyChats, SideDrawer } from "../components";
+import { useState } from "react";
 
 const ChatPage = () => {
+  const [fetchAgain, setFetchAgain] = useState(false);
   const {user} = ChatState()
 
   return (
@@ -16,8 +18,8 @@ const ChatPage = () => {
           w="100%"
           height="91.5vh"
           padding="10">
-        {user && <MyChats />}
-        {user && <ChatBox />}
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />}
       </Box>
 
     </div>
