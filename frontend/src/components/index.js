3a1bd90d5134d0e9ffe1f6c0f56c88198d5c6810
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-03-15 19:39:57 +0530

    index

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,8 +1,8 @@
 import Login from "./Authentication/Login"
 import SignUp from "./Authentication/SignUp"
 
-import ChatBox from "./misc/ChatBox"
-import MyChats from "./misc/MyChats"
+import ChatBox from "../ChatBox"
+import MyChats from "../MyChats"
 import SideDrawer from "./misc/SideDrawer"
 import ProfileModal from "./misc/ProfileModal"
 import ChatLoading from "./ChatLoading"
