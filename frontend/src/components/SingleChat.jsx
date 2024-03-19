eb7f5d2ebdb4dba9060f923b0e3dea71925eb541
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-03-19 23:59:20 +0530

    client sockets

--- frontend/src/components/SingleChat.jsx
+++ frontend/src/components/SingleChat.jsx
@@ -12,14 +12,32 @@ import ProfileModal from "./misc/ProfileModal";
 import { ChatState } from "../context/ChatProvider";
 import UpdateGroupChatModal from "./misc/UpdateGroupChatModal"
 import ScrollableChat from "./ScrollableChat";
+import io from "socket.io-client";
+import Lottie from "react-lottie";
+import animationData from "../animations/typing.json";
+
+const ENDPOINT = "http://localhost:5000"; 
+var socket,selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
 
   const [messages, setMessages] = useState([]);
   const [loading, setLoading] = useState(false);
   const [newMessage, setNewMessage] = useState("");
+  const [socketConnected, setSocketConnected] = useState(false)
+  const [typing,setTyping] = useState(false)
+  const [isTyping,setIsTyping] = useState(false)
 
   const toast = useToast();
+
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
 
   const { selectedChat, setSelectedChat, user } =  ChatState();
 
@@ -42,6 +60,8 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
       setMessages(data);
       setLoading(false);
 
+      socket.emit("join chat" , selectedChat._id)
+
     } catch (error) {
        toast({
           title: "Error Occured!",
@@ -56,6 +76,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
 
   const sendMessage = async (event) => {
      if(event.key === "Enter" && newMessage) {
+      socket.emit("stop typing" , selectedChat._id)
       try {
         const config = {
           headers : {
@@ -71,6 +92,8 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
           } , 
             config
         )
+
+        socket.emit("new message" , data)
 
         setMessages([...messages , data])
 
@@ -89,11 +112,49 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
 
   const typingHandler = (e) => {
     setNewMessage(e.target.value)
+
+    if(!socketConnected) return;
+
+    if(!typing) {
+      setTyping(true)
+      socket.emit("typing" , selectedChat._id)
+    }
+    let lastTypingTime = new Date().getTime()
+    var timerLength = 3000
+    setTimeout(() => {
+      var timeNow = new Date().getTime()
+      var timeDiff = timeNow - lastTypingTime
+
+      if(timeDiff >= timerLength && typing) {
+        socket.emit("stop typing" , selectedChat._id)
+        setTyping(false)
+      }
+    } , timerLength)
   }  
 
   useEffect(() => {
+     socket = io(ENDPOINT)
+     socket.emit("setup" , user)
+     socket.on("connected" , () => setSocketConnected(true))
+     socket.on("typing" , () => setIsTyping(true))
+     socket.on("stop typing" , () => setIsTyping(false))
+  } , [])
+
+  useEffect(() => {
     fetchMessages()
+
+    selectedChatCompare = selectedChat
   } , [selectedChat])
+
+  useEffect(() => {
+    socket.on('message recieved' , (newMessageRecieved) => {
+      if(!selectedChatCompare || selectedChatCompare._id !== newMessageRecieved.chat._id) {
+        // give notifications
+      } else {
+        setMessages([...messages,newMessageRecieved])
+      }
+    })
+  })
 
   return (
     <>
@@ -154,10 +215,21 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
             ):(
               <div className="messages">
                  <ScrollableChat messages={messages} />
+                 
               </div>
             )}
-          </Box>
-          <FormControl onKeyDown={sendMessage} isRequired mt={3}>
+            <FormControl onKeyDown={sendMessage} isRequired mt={3}>
+             {isTyping ? (
+                <div>
+                  <Lottie
+                    options={defaultOptions}
+                    width={70}
+                    style={{ marginBottom: "-15px",marginTop:"-15px", marginLeft: 0 }}
+                  />
+                </div>
+              ) : (
+                <></>
+              )}
               <Input
                 variant="filled"
                 bg="#E0E0E0"
@@ -166,6 +238,8 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                 onChange={typingHandler}
               />
           </FormControl>
+          </Box>
+          
         </>
       ) : (
         
