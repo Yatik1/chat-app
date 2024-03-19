d472bc4ceaca541aea45f7fdd88d7e3cb3f55dd6
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-03-20 00:01:25 +0530

    leave sockets

--- backend/index.js
+++ backend/index.js
@@ -66,4 +66,9 @@ io.on("connection" , (socket) => {
         })
     })
 
+    socket.off("setup", () => {
+        console.log("USER DISCONNECTED");
+        socket.leave(userData._id);
+      })
+
 })
