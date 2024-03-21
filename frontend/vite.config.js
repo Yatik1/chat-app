3dd1867e74d1ab1669bb1699d3cde9d1ffc1ca37
Yatik Srivastava <yatiksrivastava1@gmail.com>
2024-03-21 17:44:44 +0530

    deploy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server:{
     proxy:{
       '/api' : {
-        target: 'https://chat-app-9flg.onrender.com', 
+        target: 'https://chat-app-9flg.onrender.com/', 
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''), 
       },
